877e96c4ca8fb6bf7d7f335ef07677904fa39445
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-19 03:35:40 +0400

    Update cevir.py

--- userbot/modules/cevir.py
+++ userbot/modules/cevir.py
@@ -39,7 +39,7 @@ async def cevir(event):
 
         im = Image.open(foto).convert("RGB")
         im.save("sticker.png", "png")
-        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@ShreedUserBot `ilə şəkilə çevirildi.`")
+        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@TheFastUserBot `ilə şəkilə çevirildi.`")
 
         await event.delete()
         os.remove("sticker.png")
@@ -64,7 +64,7 @@ async def cevir(event):
             indir = await rep_msg.download_media()
             ses = await asyncio.create_subprocess_shell(f"ffmpeg -i '{indir}' {KOMUT[efekt]} output.mp3")
             await ses.communicate()
-            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@ShreedUserBot `ilə effekt tətbiq edildi.`")
+            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@TheFastUserBot `ilə effekt tətbiq edildi.`")
             
             await event.delete()
             os.remove(indir)
