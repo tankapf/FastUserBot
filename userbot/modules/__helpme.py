d398dcbade0007071926c1b3e1ff81584271913a
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-05-14 17:18:52 +0400

    Update __helpme.py

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -23,7 +23,7 @@ async def yardim(event):
     if tgbotusername is not None:
         results = await event.client.inline_query(
             tgbotusername,
-            "@fastuserbottt"
+            "@TheFastSup"
         )
         await results[0].click(
             event.chat_id,
